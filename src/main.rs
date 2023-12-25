49d946e1e0d0571c72757f9d42feaa7f7fd4fdb8
alaa <alaasfarhat@gmail.com>
2023-12-25 21:14:30 +0100

    added the error handling to the function that reads the csv

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use polars_core::prelude::*;
 use polars_io::prelude::*;
 
-fn example(path_to_file: &str) -> PolarsResult<DataFrame> {
+fn example(path_to_file: &str) -> Result<DataFrame,PolarsError> {
     CsvReader::from_path(path_to_file)?
             .has_header(true)
             .finish()
 }
+
+
+// the error is coded in the function for fn example, where it either returns an error or a Dataframe
 
 fn main() {
     let spots_path = "/home/alaa/repos/rustems-tests/data/spots.csv";
